808ea8b0724698b5d47690091610cb13d1ee8102
Samsara Apathika <bhinnachakra@gmail.com>
2022-12-31 17:50:23 -0800

    feat: step through events interactively

--- act-utils/ACT/main.py
+++ act-utils/ACT/main.py
@@ -1,8 +1,9 @@
 import argparse
 import logging
 import os
+import textwrap
 
-from .visualize import plot_transitions
+from .visualize import animate_transitions, plot_transitions
 
 LOGLEVEL = os.environ.get("LOGLEVEL", "INFO").upper()
 logging.basicConfig(level=LOGLEVEL)
@@ -10,11 +11,41 @@ LOGGER = logging.getLogger(__name__)
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Plot or step through events",
+        usage='use "python -m ACT --help" for more information',
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
     parser.add_argument("process", help="Name of the top-level process as described in `<process>.hac`")
-    parser.add_argument("--plot", action="store_true", default=True)
+    parser.add_argument(
+        "-p", "--plot", action="store_true", default=False, help="Plot and save the event transitions (default: False)"
+    )
+    parser.add_argument(
+        "-s",
+        "--step",
+        action="store_true",
+        default=True,
+        help="Interactively step through even transitions (default: True)",
+    )
+    parser.add_argument(
+        "-w",
+        "--window",
+        action="store",
+        type=float,
+        default=None,
+        help=textwrap.dedent(
+            """\
+            Time window to show in interactive mode (i.e., with `-s`).
+            If `None`, fit all events into the current window.
+            (default: None)
+            """
+        ),
+    )
     args = parser.parse_args()
 
     if args.plot:
         LOGGER.info(f"Plotting transitions for process: {args.process}")
         plot_transitions(args.process)
+    elif args.step:
+        LOGGER.info(f"Interactively step through event transitions for process: {args.process}")
+        animate_transitions(args.process, args.window)
